a56db450917ec8a4410bae83be3decd84086c8bf
pavel.fedorov <pavel.fedorov@vrpconsulting.com>
2020-01-16 16:13:33 +0300

    Added  functionality to display create button or rating component, small fixes in Apex controller

--- src/aura/Rating/RatingController.js
+++ src/aura/Rating/RatingController.js
@@ -1,23 +1,23 @@
 ({
     doInit: function (component, event, helper) {
 //        var result = component.
-//        helper.checkRating(component,helper);
-            var ratingElement = component.find("starRating").getElement();
-//            helper.checkRating(component);
-            helper.loadRatingElement(component, helper, ratingElement);
-            //Get current rating
-            var action = component.get("c.getCurrentRating");
-            action.setParams({
-                recordId: component.get("v.recordId")
-            });
-            action.setCallback(this, function (response) {
-                // update current rating attribute and set raty with current rating.
-
-                component.set("v.currentRating", response.getReturnValue());
-                $(ratingElement).raty('set', {score: response.getReturnValue()});
-                $(".star-rating, .loading-div, .footer-contents").toggle();
-            });
-            $A.enqueueAction(action);
+        helper.checkRating(component,helper);
+//             var ratingElement = component.find("starRating").getElement();
+// //            helper.checkRating(component);
+//             helper.loadRatingElement(component, helper, ratingElement);
+//             //Get current rating
+//             var action = component.get("c.getCurrentRating");
+//             action.setParams({
+//                 recordId: component.get("v.recordId")
+//             });
+//             action.setCallback(this, function (response) {
+//                 // update current rating attribute and set raty with current rating.
+//
+//                 component.set("v.currentRating", response.getReturnValue());
+//                 $(ratingElement).raty('set', {score: response.getReturnValue()});
+//                 $(".star-rating, .loading-div, .footer-contents").toggle();
+//             });
+//             $A.enqueueAction(action);
 
     },
 
--- src/aura/Rating/RatingHelper.js
+++ src/aura/Rating/RatingHelper.js
@@ -114,23 +114,23 @@
 //     // },
 // })
 ({
-    loadRatingElement: function(component, helper, ratingElement){
-        $( ratingElement ).raty({
-            starOff  : '/resource/RatingPlugin/images/star_off_darkgray.png',
-            starOn   : '/resource/RatingPlugin/images/star_p.png',
-            starHalf : '/resource/RatingPlugin/images/star-half-mono.png',
+    loadRatingElement: function (component, helper, ratingElement) {
+        $(ratingElement).raty({
+            starOff: '/resource/RatingPlugin/images/star_off_darkgray.png',
+            starOn: '/resource/RatingPlugin/images/star_p.png',
+            starHalf: '/resource/RatingPlugin/images/star-half-mono.png',
             half: true,
             halfShow: true,
-            click: function(score, evt) {
-                 if(score == null ) score = component.get("v.currentRating");
-                 var result = confirm('Click OK button to confirm update Rating.');
-                 if (result) {
-                     component.set("v.currentRating", score);
-                     helper.updateRating(component);
-                     $(".star-rating, .loading-div, .footer-contents").toggle();
+            click: function (score, evt) {
+                if (score == null) score = component.get("v.currentRating");
+                    var result = confirm('Click OK button to confirm update Rating.');
+                    if (result) {
+                        component.set("v.currentRating", score);
+                        helper.updateRating(component);
+                        $(".star-rating, .loading-div, .footer-contents").toggle();
 //                     helper.getCurrentRating(component);
 //                     $(".star-rating, .loading-div, .footer-contents").toggle();
-                 }
+                    }
 //                helper.getCurrentRating(component);
 //                $(".star-rating, .loading-div, .footer-contents").toggle();
 //                 if(component.get("v.currentRating") != score ){
@@ -146,20 +146,21 @@
 //                         return false;
 //                     }
 //                 }
-             },
+            },
         });
     },
-    updateRating : function(component){
+    updateRating: function (component) {
         var ratingElement = component.find("starRating").getElement();
         // update record with new rating.
         var action = component.get("c.updateRating");
         action.setParams({
-            recordId : component.get("v.recordId"),
-            rating : component.get("v.currentRating")
+            recordId: component.get("v.recordId"),
+            rating: component.get("v.currentRating")
         });
-        action.setCallback(this, function( response ){
-            component.set( "v.currentRating", response.getReturnValue());
-            $(ratingElement).raty('set', { score: response.getReturnValue() });
+        action.setCallback(this, function (response) {
+            component.set("v.currentRating", response.getReturnValue());
+            console.log(response.getReturnValue());
+            $(ratingElement).raty('set', {score: response.getReturnValue()});
             $(".star-rating, .loading-div, .footer-contents").toggle();
         });
         var toastEvent = $A.get("e.force:showToast");
@@ -172,15 +173,16 @@
         $A.enqueueAction(action);
     },
 
-    insertRating : function (component,helper) {
+    insertRating: function (component, helper) {
         var self = this;
         var action = component.get("c.createNewRating");
         action.setParams({
-            recordId : component.get("v.recordId")
+            recordId: component.get("v.recordId")
         });
-        action.setCallback(this, function( response ){
-            component.set( "v.currentRating", response.getReturnValue());
-            component.set("v.showBool",true);
+        action.setCallback(this, function (response) {
+            component.set("v.currentRating", response.getReturnValue());
+            component.set("v.showBool", true);
+            self.checkRating(component, helper);
 
         });
         var toastEvent = $A.get("e.force:showToast");
@@ -193,44 +195,44 @@
         $A.enqueueAction(action);
     },
 
-//     getCurrentRating : function (component,helper) {
-//         var ratingElement = component.find("starRating").getElement();
-//         var self = this;
-//         self.loadRatingElement( component, helper, ratingElement );
-// //        self.helper.loadRatingElement( component, helper, ratingElement );
-//         //Get current rating
-//         var action = component.get("c.getCurrentRating");
-//         action.setParams({
-//             recordId : component.get("v.recordId")
-//         });
-//         action.setCallback(this, function( response ){
-//             // update current rating attribute and set raty with current rating.
-// //            self.loadRatingElement(component, helper, ratingElement );
-//             component.set("v.currentRating", response.getReturnValue());
-//             console.log(component.get("v.currentRating"));
-//             $(ratingElement).raty('set', { score: response.getReturnValue() });
-//             $(".star-rating, .loading-div, .footer-contents").toggle();
-//         });
-//         $A.enqueueAction(action);
-//     },
-    checkRating : function (component,helper) {
+    getCurrentRating: function (component, helper) {
+        var ratingElement = component.find("starRating").getElement();
         var self = this;
-       var action = component.get("c.checkRating");
-       action.setParams({
-           recordId : component.get("v.recordId")
-       });
-       action.setCallback(this, function( response ){
-           var result = response.getReturnValue();
-           console.log(result);
-           if(result) {
-               component.set("v.showBool",result);
-               self.getCurrentRating(component,helper);
-           } else {
-               component.set("v.showBool",false);
-           }
+        self.loadRatingElement(component, helper, ratingElement);
+//        self.helper.loadRatingElement( component, helper, ratingElement );
+        //Get current rating
+        var action = component.get("c.getCurrentRating");
+        action.setParams({
+            recordId: component.get("v.recordId")
+        });
+        action.setCallback(this, function (response) {
+            // update current rating attribute and set raty with current rating.
+//            self.loadRatingElement(component, helper, ratingElement );
+            component.set("v.currentRating", response.getReturnValue());
+            console.log(component.get("v.currentRating"));
+            $(ratingElement).raty('set', {score: response.getReturnValue()});
+            $(".star-rating, .loading-div, .footer-contents").toggle();
+        });
+        $A.enqueueAction(action);
+    },
+    checkRating: function (component, helper) {
+        var self = this;
+        var action = component.get("c.checkRating");
+        action.setParams({
+            recordId: component.get("v.recordId")
+        });
+        action.setCallback(this, function (response) {
+            var result = response.getReturnValue();
+            console.log(result);
+            if (result) {
+                component.set("v.showBool", result);
+                self.getCurrentRating(component, helper);
+            } else {
+                component.set("v.showBool", false);
+            }
 
-       });
-       $A.enqueueAction(action);
+        });
+        $A.enqueueAction(action);
     },
 
 })
